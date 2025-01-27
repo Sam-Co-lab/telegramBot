8d1fb92ddd15b9c1f93b94e69e4f4053c5f4c1ce
Sam-Co-lab <samaulins@gmail.com>
2025-01-27 15:15:53 +0530

    Update telegramBot.py

--- telegramBot.py
+++ telegramBot.py
@@ -105,7 +105,7 @@ def handle_duration(update: Update, context: CallbackContext) -> None:
                     can_send_media_messages=False,
                     can_send_polls=False,
                     can_send_other_messages=False,
-                    can add_web_page_previews=False,
+                    can_add_web_page_previews=False,
                     can_change_info=False,
                     can_invite_users=False,
                     can_pin_messages=False)
